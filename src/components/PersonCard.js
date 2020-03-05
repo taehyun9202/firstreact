29cb2700110dab613cb2acdd3ae7f939f0002962
taehyun9202 <60115779+taehyun9202@users.noreply.github.com>
2020-03-05 11:10:49 -0500

    Update PersonCard.js

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -6,10 +6,23 @@ class PersonCard extends React.Component {
         return(
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>age: {this.props.age}</p>
+                <p>age: {Number(this.props.age)+Number(this.state.clickCount)}</p>
                 <p>hair color: {this.props.hairColor}</p>
+                <button onClick={this.handleClick}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
-        );
+        ); 
     }
+    constructor(props){
+        super(props);
+        this.state = {
+            clickCount: 0
+        }
+    }
+    handleClick = () => {
+        this.setState({
+            clickCount: this.state.clickCount + 1
+        })
+    }
+
 }
 export default PersonCard
