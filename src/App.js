5d7a45b66771cac4fac28fecf98a146e1d01b950
taehyun9202 <60115779+taehyun9202@users.noreply.github.com>
2020-03-05 12:59:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
-import PersonCard from './components/PersonCard';
+import PersonCard from './components/PersonCard'; // personalcard
+import FunctionalPersonCard from './components/FunctionalPersonCard'; // functionalpersoncard
 
 
 // --Helloworld--
