b74d931695cdcebf95c851b12be1e1ab37052d39
taehyun9202 <60115779+taehyun9202@users.noreply.github.com>
2020-03-05 13:26:11 -0500

    Update FunctionalPersonCard.js

--- src/components/FunctionalPersonCard.js
+++ src/components/FunctionalPersonCard.js
@@ -3,21 +3,33 @@ import React, { useState } from 'react';
 
 
 const FunctionalPersonCard = props => {
-return(
+    // -- way 1 --
+    const [clickCount, age] = useState(0);
+
+    const Increament = () => {
+        age(clickCount + 1);
+    }
+
+    return(
     <div>
         <h1>{props.lastName}, {props.firstName}</h1>
-        <p>age: {props.age}</p>
+        <p>age: {props.age + clickCount}</p>
         <p>hair color: {props.hairColor}</p>
-        {/* <button onClick={this.Increament}>Birthday Button for {props.firstName} {props.lastName}</button> */}
+        <button onClick={Increament}>Birthday Button for {props.firstName} {props.lastName}</button>
     </div>
+
+    // -- way2 --
+    // const [clickCount, age] = useState(0);
+    // return(
+    // <div>
+    //     <h1>{props.lastName}, {props.firstName}</h1>
+    //     <p>age: {props.age + clickCount}</p>
+    //     <p>hair color: {props.hairColor}</p>
+    //     <button onClick={ () => age(clickCount +1) }>Birthday Button for {props.firstName} {props.lastName}</button>
+    // </div>
 )
 }
-//    const [clickCount, age] = useState(props.age);
-
-//     const Increament = () => {
-//         age(clickCount + 1);
-//     }
-// }
+    
 
 
 export default FunctionalPersonCard
